f0622025b2fae70b318db4dbf706a520e4f26289
KAIZER445 <prashanttamang861@gmail.com>
2026-07-21 23:02:09 +0545

    setup: configure prisma

--- src/core/core.service.ts
+++ src/core/core.service.ts
@@ -4,7 +4,7 @@ import { customAlphabet } from 'nanoid';
 
 @Injectable()
 export class CoreService {
-    private readonly generateId = customAlphabet('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ', 6);
+    private readonly generateId = customAlphabet('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz', 8);
     urlShortner(baseApp: string): string {
         const newUrl = baseApp + this.generateId()
         return (newUrl + "")
